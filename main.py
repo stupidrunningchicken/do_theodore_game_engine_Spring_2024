b9023aeef8f0b7f29b3d8b742ce6a6c3f120ae96
stupidrunningchicken <160263599+stupidrunningchicken@users.noreply.github.com>
2024-02-16 09:00:47 -0800

    Hello GitHub!

---
+++ main.py
@@ -0,0 +1,124 @@
+# This file was created by: Theodore Do
+
+
+# import libraries
+import pygame as pg
+from settings import *
+from sprites import *
+import sys
+from os import path 
+from random import randint
+
+# create a game class 
+class Game:
+    # behold the methods...
+    def __init__(self):
+        pg.init()
+        # 
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        # 
+        pg.display.set_caption("My First Video Game")
+        # 
+        self.clock = pg.time.Clock()
+        pg.key.set_repeat(500, 100)
+        self.running = True
+        # later on we'll story game info with this
+        self.load_data()
+    def load_data(self):
+        game_folder = path.dirname(__file__)
+        self.map_data = []
+        '''
+        The with statement is a context manager in Python. 
+        It is used to ensure that a resource is properly closed or released 
+        after it is used. This can help to prevent errors and leaks.
+        '''
+        with open(path.join(game_folder, 'map.txt'), 'rt') as f:
+            for line in f:
+                print(line)
+                self.map_data.append(line)
+                print(self.map_data)
+    def new(self):
+        self.all_sprites = pg.sprite.Group()
+        self.walls = pg.sprite.Group()
+        # self.player = Player(self, 10, 10)
+        # self.all_sprites.add(self.player)
+        # for x in range(10, 20):
+        #     Wall(self, x, 5)
+        for row, tiles in enumerate(self.map_data):
+            print(row)
+            for col, tile in enumerate(tiles):
+                print(col)
+                if tile == 'x':
+                    print("a wall at", row, col)
+                    Wall(self, col, row)
+                if tile == 'p':
+                    self.player = Player(self, col, row)
+    def run(self):
+        self.playing = True
+        while self.playing:
+            self.dt = self.clock.tick(FPS) / 1000
+            # this is input
+            self.events()
+            # this is processing
+            self.update()
+            # this output
+            self.draw()
+
+    def quit(self):
+        pg.quit()
+        sys.exit()
+    # methods
+    def input(self): 
+        pass
+    def update(self):
+        self.all_sprites.update()
+    
+    def draw_grid(self):
+        for x in range(0, WIDTH, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (x, 0), (x, HEIGHT))
+        for y in range(0, HEIGHT, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH, y))
+
+    def draw(self):
+        self.screen.fill(BGCOLOR)
+        self.draw_grid()
+        self.all_sprites.draw(self.screen)
+        pg.display.flip()
+
+    def events(self):
+            # listening for events
+            for event in pg.event.get():
+                # when you hit the red x the window closes the game ends
+                if event.type == pg.QUIT:
+                    self.quit()
+                    print("the game has ended..")
+                # keyboard events
+                # if event.type == pg.KEYDOWN:
+                #     if event.key == pg.K_LEFT:
+                #         self.player.move(dx=-1)
+                # if event.type == pg.KEYDOWN:
+                #     if event.key == pg.K_RIGHT:
+                #         self.player.move(dx=1)
+                # if event.type == pg.KEYDOWN:
+                #     if event.key == pg.K_UP:
+                #         self.player.move(dy=-1)
+                # if event.type == pg.KEYDOWN:
+                #     if event.key == pg.K_DOWN:
+                #         self.player.move(dy=1)
+    def show_start_screen(self):
+        pass
+    def show_go_screen(self):
+        pass
+####################### Instantiate game... ###################
+g = Game()
+# g.show_go_screen()
+while True:
+    g.new()
+    g.run()
+    # g.show_go_screen()
+
+
+#Questions:
+#1: What does line 18 specifically do?
+#2: What does "pass" mean?
+#3: What does line 25 do? 
---
+++ sprites.py
@@ -0,0 +1,130 @@
+# This file was created by: Theodore Do
+
+
+import pygame as pg
+from settings import *
+
+ 
+# write a player class
+class Player(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pg.Surface((TILESIZE, TILESIZE))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.vx, self.vy = 0, 0
+        self.x = x * TILESIZE
+        self.y = y * TILESIZE
+
+    def get_keys(self):
+        self.vx, self.vy = 0, 0
+        keys = pg.key.get_pressed()
+        if keys[pg.K_LEFT] or keys[pg.K_a]:
+            self.vx = -PLAYER_SPEED
+            print(self.rect.x)
+            print(self.rect.y)
+        if keys[pg.K_RIGHT] or keys[pg.K_d]:
+            self.vx = PLAYER_SPEED
+        if keys[pg.K_UP] or keys[pg.K_w]:
+            self.vy = -PLAYER_SPEED
+        if keys[pg.K_DOWN] or keys[pg.K_s]:
+            self.vy = PLAYER_SPEED
+            
+    def collide_with_walls(self, dir):
+        if dir == 'x':
+            hits = pg.sprite.spritecollide(self, self.game.walls, False )
+            if hits:
+                if self.vx > 0:
+                    self.x = hits[0].rect.left - self.rect.width
+                if self.vx < 0:
+                    self.x = hits[0].rect.right
+                self.vx = 0
+                self.rect.x = self.x
+        if dir == 'y':
+            hits = pg.sprite.spritecollide(self, self.game.walls, False )
+            if hits:
+                if self.vy > 0:
+                    self.y = hits[0].rect.top - self.rect.height
+                if self.vy < 0:
+                    self.y = hits[0].rect.bottom
+                self.vy = 0
+                self.rect.y = self.y
+    # old motion
+    # def move(self, dx=0, dy=0):
+    #     self.x += dx
+    #     self.y += dy
+
+    # UPDATE THE UPDATE
+    def update(self):
+        # self.rect.x = self.x
+        # self.rect.y = self.y
+        self.get_keys()
+        self.x += self.vx * self.game.dt
+        self.y += self.vy * self.game.dt
+        self.rect.x = self.x
+        self.collide_with_walls('x')
+        self.rect.y = self.y
+        self.collide_with_walls('y')
+        # self.rect.x = self.x * TILESIZE
+        # self.rect.y = self.y * TILESIZE
+
+class Wall(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites, game.walls
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pg.Surface((TILESIZE, TILESIZE))
+        self.image.fill(YELLOW)
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+        self.rect.x = x * TILESIZE
+        self.rect.y = y * TILESIZE
+        self.speed = 0
+    def update(self):
+        # self.rect.x += 1
+        self.rect.x += TILESIZE * self.speed
+        # self.rect.y += TILESIZE * self.speed
+        if self.rect.x > WIDTH or self.rect.x < 0:
+            self.speed *= -1
+        # if self.rect.y > HEIGHT or self.rect.y < 0:
+        #     self.speed *= -1
+
+class Mob(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites, game.mobs
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pg.Surface((TILESIZE, TILESIZE))
+        self.image.fill(YELLOW)
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+        self.rect.x = x * TILESIZE
+        self.rect.y = y * TILESIZE
+        self.speed = 1
+    def update(self):
+        # self.rect.x += 1
+        self.rect.x += TILESIZE * self.speed
+        # self.rect.y += TILESIZE * self.speed
+        if self.rect.x > WIDTH or self.rect.x < 0:
+            self.speed *= -1
+        # if self.rect.y > HEIGHT or self.rect.y < 0:
+        #     self.speed *= -1
+
+
+           
+class Bigger(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites, game.walls
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pg.Surface((TILESIZE, TILESIZE))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.x = y
+        self.y = y
+        self.rect.x = x * TILESIZE
+        self. rect.y = y *TILESIZE
